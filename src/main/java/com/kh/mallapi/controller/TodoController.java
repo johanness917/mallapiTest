126a571963487903bfc75b818c7d37ef7bf80a3a
johanness917 <jhhan917@gmail.com>
2026-03-10 10:49:37 +0900

    [수정] 스프링 부트를 이용해서 RESTful한 API 서버 구성을 위한 수정

--- src/main/java/com/kh/mallapi/controller/TodoController.java
+++ src/main/java/com/kh/mallapi/controller/TodoController.java
@@ -1,7 +1,13 @@
 package com.kh.mallapi.controller;
 
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +25,7 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 @RequestMapping("/api/todo")
 public class TodoController {
-	
+
 	private final TodoService service;
 
 	@GetMapping("/{tno}")
@@ -31,5 +37,27 @@ public class TodoController {
 	public PageResponseDTO<TodoDTO> list(PageRequestDTO pageRequestDTO) {
 		log.info(pageRequestDTO);
 		return service.list(pageRequestDTO);
+	}
+
+	@PostMapping("/")
+	public Map<String, Long> register(@RequestBody TodoDTO todoDTO) {
+		log.info("TodoDTO: " + todoDTO);
+		Long tno = service.register(todoDTO);
+		return Map.of("TNO", tno);
+	}
+
+	@PutMapping("/{tno}")
+	public Map<String, String> modify(@PathVariable(name = "tno") Long tno, @RequestBody TodoDTO todoDTO) {
+		todoDTO.setTno(tno);
+		log.info("Modify: " + todoDTO);
+		service.modify(todoDTO);
+		return Map.of("RESULT", "SUCCESS");
+	}
+
+	@DeleteMapping("/{tno}")
+	public Map<String, String> remove(@PathVariable(name = "tno") Long tno) {
+		log.info("Remove: " + tno);
+		service.remove(tno);
+		return Map.of("RESULT", "SUCCESS");
 	}
 }
